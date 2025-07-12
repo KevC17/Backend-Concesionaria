e412864e3aede3cd0139694eb60ed47697243f58
Kevin Cóndor <84334368+KevinEC17@users.noreply.github.com>
2025-07-12 16:32:26 -0500

    Fix: Enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
 
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
 
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
-bootstrap(); 
+bootstrap();
